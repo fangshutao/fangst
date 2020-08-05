ca4c25525d7a779edf306881d5d15c47adaa45f6
FanSirSan <fanweihua363712208@qq.com>
2020-08-05 17:34:37 +0800

    1、【功能】完成框架介绍和数据流转 2、【功能】完善框架结构，优化框架代码

--- src/router/handle/router.js
+++ src/router/handle/router.js
@@ -1,13 +1,7 @@
-/** @format */
-
 'use strict'
 /**
  * Created by weiChow on 2020/06/30.
- * @author weiChow
- * 入口路由
- * export default Router;
  */
-
 import asyncComponent from '@/common/tool/asyncComponent' // 异步加载
 const Router = [
   {
